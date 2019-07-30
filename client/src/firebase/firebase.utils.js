f4497e746bd9c4675009a7540aa5f2a48f25d741
curgery <androidgeek54@gmail.com>
2019-07-30 15:29:05 -0400

    separating the CRA app into a client folder, added express server, to help handle backend stripe account.

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -3,7 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
- 
+  apiKey: "AIzaSyClnbgzpeQ6wqgykmR-WE7Rxrs8OW-b_FU",
+  authDomain: "crwn-db-68598.firebaseapp.com",
+  databaseURL: "https://crwn-db-68598.firebaseio.com",
+  projectId: "crwn-db-68598",
+  storageBucket: "",
+  messagingSenderId: "1095430907604",
+  appId: "1:1095430907604:web:4f082b3d88f71550"
 };
 
 firebase.initializeApp(config);
