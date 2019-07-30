94a9a99456168427173c2ac4a6a2319f60740c00
curgery <androidgeek54@gmail.com>
2019-07-30 15:09:00 -0400

    Remodeled app to complete backend connection for credit card payments, new Client folder.

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout';
-
+import axios from 'axios';
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = '';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
-    }
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful')
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert(
+                'there was an issue with your payment. Please make sure you use the provided credit card information provided. '
+            );
+        });
+    };
 
     return (
         <StripeCheckout
